a52c801dc47eefe8515244925d5ad665f89d84aa
eddybrayam <kcbrayam@gmail.com>
2025-11-20 17:19:21 -0500

    Vista de Carrito Compras

--- app/src/main/java/com/example/easyshop/AppUtil.kt
+++ app/src/main/java/com/example/easyshop/AppUtil.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.firestore
 
 
@@ -32,6 +33,36 @@ object AppUtil {
                             showToast(context, message = "Item added to the cart")
                         }else{
                             showToast(context, message = "Failed adding item to the cart")
+                        }
+                    }
+            }
+        }
+    }
+    fun removeFromCart(context: Context, productId : String,removeAll : Boolean = false){
+
+        val userDoc = Firebase.firestore.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid!!)
+
+        userDoc.get().addOnCompleteListener {
+            if(it.isSuccessful){
+                val currentCart = it.result.get("cartItems") as? Map<String, Long> ?: emptyMap()
+                val currentQuantity = currentCart[productId]?:0
+                val updatedQuantity = currentQuantity - 1;
+
+                val updatedCart =
+                    if (updatedQuantity<=0 ||removeAll)
+                        mapOf("cartItems.$productId" to FieldValue.delete())
+                    else
+                        mapOf("cartItems.$productId" to updatedQuantity)
+
+                    mapOf("cartItems.$productId" to updatedQuantity)
+
+                userDoc.update(updatedCart)
+                    .addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showToast(context, message = "Item remove to the cart")
+                        }else{
+                            showToast(context, message = "Failed removing item to the cart")
                         }
                     }
             }
--- app/src/main/java/com/example/easyshop/pages/CartPage.kt
+++ app/src/main/java/com/example/easyshop/pages/CartPage.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,6 +16,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.request.Disposable
 import com.example.easyshop.components.CartItemView
 import com.example.easyshop.model.UserModel
 import com.google.firebase.Firebase
@@ -27,17 +29,21 @@ fun CartPage(modifier: Modifier = Modifier) {
         mutableStateOf(UserModel())
     }
 
-    LaunchedEffect(key1 = Unit) {
-        Firebase.firestore.collection("users")
+    DisposableEffect(key1 = Unit) {
+        var listener = Firebase.firestore.collection("users")
             .document(FirebaseAuth.getInstance().currentUser?.uid!!)
-            .get().addOnCompleteListener() {
-                if (it.isSuccessful) {
-                    val result = it.result.toObject(UserModel::class.java)
+            .addSnapshotListener{it, _->
+                if (it!=null) {
+                    val result = it.toObject(UserModel::class.java)
                     if (result != null) {
                         userModel.value = result
                     }
                 }
             }
+        onDispose {
+            listener.remove()
+        }
+
     }
 
     Column(
@@ -52,7 +58,7 @@ fun CartPage(modifier: Modifier = Modifier) {
             )
         )
         LazyColumn {
-            items(userModel.value.cartItems.toList()) { (productId, qty) ->
+            items(userModel.value.cartItems.toList(),key = { it.first}) { (productId, qty) ->
                 CartItemView(productId = productId, qty = qty)
             }
         }
